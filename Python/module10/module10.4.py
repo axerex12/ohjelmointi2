d7330e7e4b139803f1177d40119771556ddfa98c
axerex12 <axel.aaldijk12@gmail.com>
2024-10-23 16:23:49 +0300

    tehty

--- Python/module10/module10.4.py
+++ Python/module10/module10.4.py
@@ -1,34 +1,32 @@
 import random
 
-class kilpailu:
-    def __init__(self, nimi, pituuskm, osallistuja_lista ):
+class Kilpailu:
+    def __init__(self, nimi, pituuskm, osallistuja_lista):
         self.nimi = nimi
         self.pituuskm = pituuskm
-        self.osallistuja_lista= osallistuja_lista
+        self.osallistuja_lista = osallistuja_lista
 
     def tunti_kuluu(self):
         for auto in self.osallistuja_lista:
             randkiihd = random.randint(-10, 15)
             auto.kiihdytä(randkiihd)
             auto.kulje(1)
-            self.kilpailu_ohi()
+
     def tulosta_tilanne(self):
+        print(f"{'Rekisteri':<10}{'Huippunopeus':<15}{'Nopeus':<15}{'Kuljettu matka':<15}")
+        print("="*55)
         for auto in self.osallistuja_lista:
-            print(f"{auto.rekis:<8}{auto.huippunopeus:<15}{auto.nopeus:<15}{auto.kuljettumatka:<15}")
+            print(f"{auto.rekis:<10}{auto.huippunopeus:<15}{auto.nopeus:<15}{auto.kuljettumatka:<15}")
 
     def kilpailu_ohi(self):
-        raceon = True
-        while raceon:
-
-            for auto in self.osallistuja_lista:
-                if auto.kuljettumatka > self.pituuskm:
-                    print(f"Auto {auto.rekis} on saavuttanut yli 10 000 km ja race on over")
-                    raceon = False
-                    break
+        for auto in self.osallistuja_lista:
+            if auto.kuljettumatka >= self.pituuskm:
+                print(f"Auto {auto.rekis} on saavuttanut maalin!")
+                return True
+        return False
 
 
-
-class auto:
+class Auto:
 
     def __init__(self, rekis, huippunopeus):
         self.rekis = rekis
@@ -40,19 +38,31 @@ class auto:
         if self.nopeus + nopeuden_muutos > self.huippunopeus:
             self.nopeus = self.huippunopeus
         elif self.nopeus + nopeuden_muutos < 0:
-            self.nopeus =0
+            self.nopeus = 0
         else:
             self.nopeus += nopeuden_muutos
+
     def kulje(self, tuntimäärä):
         self.kuljettumatka += self.nopeus * tuntimäärä
 
 
-autot = []
 
-for i in range(1,11):
+autot = []
+for i in range(1, 11):
     rekkari = f"ABC-{i}"
     maxnopeus = random.randint(100, 200)
-    auuto = auto(rekkari, maxnopeus)
-    autot.append(auuto)
+    autot.append(Auto(rekkari, maxnopeus))
 
-kilpailu("Suuri romuralli",8000, autot)
+
+kisa = Kilpailu("Suuri romuralli", 8000, autot)
+
+
+tunti = 0
+while not kisa.kilpailu_ohi():
+    kisa.tunti_kuluu()
+    tunti += 1
+    if tunti % 10 == 0:
+        kisa.tulosta_tilanne()
+
+print(f"Kilpailu päättyi {tunti} tunnin jälkeen.")
+kisa.tulosta_tilanne()
