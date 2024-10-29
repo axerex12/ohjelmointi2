39b1b5dd65300ddec55611b786e5a67a76059fb7
axerex12 <axel.aaldijk12@gmail.com>
2024-10-29 14:20:29 +0200

    .

--- Python/module12/module12.2.py
+++ Python/module12/module12.2.py
@@ -1,20 +1,17 @@
 import requests
 import json
 
-def ctof(lämpöf):
-
-
 API_Key = "d43fb48d7c36a2d1404e5fa75121fb5d"
-#city_name = input("Anna kaupungin nimi englanniksi: ")
-city_name = "London"
+city_name = input("Anna kaupungin nimi englanniksi: ")
 
-vastaus = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_Key}").json()
+
+vastaus = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_Key}&units=metric").json()
 print(vastaus)
 sää = vastaus['weather']
 säää = sää[0]
 lämptila = vastaus["main"]
-lämptilaf = lämptila["temp"]
+lämptilac = lämptila["temp"]
 
 
 print(säää["description"])
-print(lämptilac)
+print(f"{lämptilac} celsius astetta")
